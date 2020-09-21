a6882c5197f4762d42c967ea20e36ea4c371c52d
nomadbard916 <neonnova916@gmail.com>
2020-09-21 19:25:12 +0800

    create and list answer endpoints

---
+++ QuestionTime/questions/api/views.py
@@ -0,0 +1,69 @@
+from rest_framework import generics, status, viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from questions.api.permissions import IsAuthorOrReadOnly
+from questions.api.serializers import AnswerSerializer, QuestionSerializer
+from questions.models import Answer, Question
+
+
+class AnswerCreateAPIView(generics.CreateAPIView):
+    """Allow users to answer a question instance if they haven't already."""
+
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        request_user = self.request.user
+        kwarg_slug = self.kwargs.get("slug")
+        question = get_object_or_404(Question, slug=kwarg_slug)
+
+        if question.answers.filter(author=request_user).exists():
+            raise ValidationError("You have already answered this Question!")
+
+        serializer.save(author=request_user, question=question)
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    """Provide CRUD +L functionality for Question."""
+
+    queryset = Question.objects.all().order_by("-created_at")
+    lookup_field = "slug"
+    serializer_class = QuestionSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class AnswerCreateAPIView(generics.CreateAPIView):
+    """Allow users to answer a question instance if they haven't already."""
+
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        request_user = self.request.user
+        kwarg_slug = self.kwargs.get("slug")
+        question = get_object_or_404(Question, slug=kwarg_slug)
+
+        if question.answers.filter(author=request_user).exists():
+            raise ValidationError("You have already answered this Question!")
+
+        serializer.save(author=request_user, question=question)
+
+
+class AnswerListAPIView(generics.ListAPIView):
+    """Provide the answers queryset of a specific question instance."""
+
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        kwarg_slug = self.kwargs.get("slug")
+        return Answer.objects.filter(question__slug=kwarg_slug).order_by("-created_at")
